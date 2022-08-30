fd5826cab68b61c5d6b71e75e1cdfba840a6061e
AmanzholAbdykerimov <amanzhol.abdykerimov@mail.ru>
2022-08-30 16:10:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        int t=60;
+        int[] arr={1,2,3,4,5,6,7,8,9};
+        Stream.of(arr);
+        Arrays.stream(arr).filter(x->x%2==0).map(x->x*x).filter(x-> x>t).forEach(System.out::println);
+        //Arrays.stream(arr).filter(x->Math.max(x,x)).forEach(System.out::println);
+    }}
+
+
+    //Integer массивден жуп сандарды терип алыныздар жана алардын квадраттарынын эн чонун табыныз.
